c985626648f47c318364a961c7ad4f3b61071673
Jared.K <3268576+jaredkotoff@users.noreply.github.com>
2024-05-31 15:34:55 -0700

    Fix running locally

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,12 +4,14 @@ exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     resolve: {
       fallback: {
+        'process/browser': require.resolve('process/browser'),
         crypto: require.resolve('crypto-browserify'),
         stream: require.resolve('stream-browserify'),
       },
     },
     plugins: [
       new webpack.ProvidePlugin({
+        process: 'process/browser',
         Buffer: [require.resolve('buffer/'), 'Buffer'],
       }),
     ],
